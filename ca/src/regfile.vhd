6b4eb82cd51df40859e7fbea432032a60946904c
Michael Reitgruber <reitgruber.michael@gmail.com>
2016-12-07 18:38:16 +0100

    Register file

--- ca/src/regfile.vhd
+++ ca/src/regfile.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 use work.core_pack.all;
 
 entity regfile is
-	
+
 	port (
 		clk, reset       : in  std_logic;
 		stall            : in  std_logic;
@@ -18,7 +18,50 @@ entity regfile is
 end regfile;
 
 architecture rtl of regfile is
+	type regfile_type is array(0 to REG_COUNT-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
+
+	signal rdaddr1_int, rdaddr2_int: std_logic_vector(REG_BITS-1 downto 0);
+	signal wraddr_int: std_logic_vector(REG_BITS-1 downto 0) := (others => '0');
+	signal regfile: regfile_type;
 
 begin  -- rtl
-	
+
+  readwrite: process(rdaddr1_int, rdaddr2_int, wraddr_int, wrdata, regwrite) is
+	begin
+		if regwrite = '1' then
+			if wraddr_int /= (REG_BITS -1 downto 0 => '0') then
+				if wraddr_int = rdaddr1_int then
+					rddata1 <= wrdata;
+				end if;
+				if wraddr_int = rdaddr2_int then
+					rddata2 <= wrdata;
+				end if;
+				regfile(to_integer(unsigned(wraddr_int))) <= wrdata;
+			end if;
+		else
+			if rdaddr1_int = (REG_BITS-1 downto 0 => '0') then
+				rddata1 <= (DATA_WIDTH-1 downto 0 => '0');
+			else
+				rddata1 <= regfile(to_integer(unsigned(rdaddr1_int)));
+			end if;
+			if rdaddr2_int = (REG_BITS-1 downto 0 => '0') then
+				rddata2 <= (DATA_WIDTH-1 downto 0 => '0');
+			else
+				rddata2 <= regfile(to_integer(unsigned(rdaddr2_int)));
+			end if;
+		end if;
+	end process;
+
+	latch: process(clk, reset) is
+	begin
+		if reset = '0' then
+			rdaddr1_int <= (others => '0');
+			rdaddr2_int <= (others => '0');
+			wraddr_int <= (others => '0');
+		elsif rising_edge(clk) and (not(stall = '1'))then
+			rdaddr1_int <= rdaddr1;
+			rdaddr2_int <= rdaddr2;
+			wraddr_int <= wraddr;
+		end if;
+	end process;
 end rtl;
---
+++ ca/src/testbench_regfile.vhd
@@ -0,0 +1,79 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.core_pack.all;
+use work.op_pack.all;
+
+entity testbench_regfile is
+end entity;
+
+architecture beh of testbench_regfile is
+  constant CLK_PERIOD: time := 20 ns;
+  signal clk, reset, stall, regwrite: std_logic;
+  signal rdaddr1, rdaddr2, wraddr: std_logic_vector(REG_BITS-1 downto 0);
+  signal rddata1, rddata2, wrdata: std_logic_vector(DATA_WIDTH-1 downto 0);
+
+begin
+
+  reg: entity work.regfile
+  port map(
+    clk => clk,
+    reset => reset,
+    stall => stall,
+    rdaddr1 => rdaddr1,
+    rdaddr2 => rdaddr2,
+    rddata1 => rddata1,
+    rddata2 => rddata2,
+    wraddr => wraddr,
+    wrdata => wrdata,
+    regwrite => regwrite
+  );
+
+  clk_gen: process is
+  begin
+    clk <= '1';
+    wait for CLK_PERIOD/2;
+    clk <= '0';
+    wait for CLK_PERIOD/2;
+  end process;
+
+  rs_gen: process is
+  begin
+    reset <= '0';
+    wait for 2*CLK_PERIOD;
+    reset <= '1';
+    wait;
+  end process;
+
+  regfile_test: process is
+  begin
+    --test write
+    wait until rising_edge(reset);
+    wraddr <= "11111";
+    wait until rising_edge(clk);
+    wrdata <= x"aaaaaaaa";
+    regwrite <= '1';
+    wait until rising_edge(clk);
+    wraddr <= "11110";
+    wait until rising_edge(clk);
+    wrdata <= x"aaaaaaaa";
+    wait until rising_edge(clk);
+
+    --test simultaneous read and write
+    rdaddr1 <= "11111";
+    wraddr <= "11111";
+    wait until rising_edge(clk);
+    wrdata <= x"0000aaaa";
+    regwrite <= '1';
+    wait until rising_edge(clk);
+    regwrite <= '0';
+    wait until rising_edge(clk);
+
+    --test read i guess
+    rdaddr1 <= "11111";
+    rdaddr2 <= "11110";
+    wait until rising_edge(clk);
+  end process;
+
+end architecture beh;
